8b91f03c6d333c9e4b1be66eae0c581638e98a7b
Cristian Olivares (visibait) <grafoplays@gmail.com>
2024-10-23 09:41:54 +0200

    upload project template

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/ping', async (request, reply) => {
+  return 'pong\n'
+})
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err)
+  }
+  console.log(`Server listening at ${address}`)
+})
+
