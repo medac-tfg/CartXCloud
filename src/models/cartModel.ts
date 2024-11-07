bc72c371a892c9ae9726507d90cf178620f73670
Cristian Olivares (visibait) <grafoplays@gmail.com>
2024-11-07 13:11:35 +0100

    tweak(CartModel): Update schema to add shop

--- src/models/cartModel.ts
+++ src/models/cartModel.ts
@@ -1,13 +1,19 @@
-import { Schema, model } from "mongoose";
+import { ObjectId, Schema, model } from "mongoose";
 
 interface Cart {
   name: string;
+  shop: ObjectId;
   createdAt: Date;
 }
 
 const cartSchema = new Schema<Cart>({
   name: {
     type: String,
+    required: true,
+  },
+  shop: {
+    type: Schema.Types.ObjectId,
+    ref: "Shop",
     required: true,
   },
   createdAt: {
